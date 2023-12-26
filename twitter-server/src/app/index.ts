bdf0ef44a60f4776e811fcd36c17f9ceae1ab15b
Rithik53 <rithikkumar53@gmail.com>
2023-12-26 17:46:20 +0530

    setting up prisam orm and postgresql

--- twitter-server/src/app/index.ts
+++ twitter-server/src/app/index.ts
@@ -2,11 +2,18 @@ import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import bodyParser from 'body-parser';
+import { prismaclient } from "../clients/db";
 
 
 export async function initserver() {
     const app = express();
     app.use(bodyParser.json());
+
+    prismaclient.user.create({
+        data: {
+            
+        }
+    })
     const graphqlServer = new ApolloServer({
         typeDefs:`
         type Query {
