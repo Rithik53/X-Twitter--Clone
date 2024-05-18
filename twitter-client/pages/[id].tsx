6bfbc833cda365e57180b1e74c1a193b85e23394
Rithik53 <rithikkumar53@gmail.com>
2024-05-18 13:19:01 +0530

    mobile responsive and created layout
    This is mobile responsive and layout could be reused

---
+++ twitter-client/pages/[id].tsx
@@ -0,0 +1,13 @@
+import Twitterlayout from "@/components/FeedCard/Layout/TwitterLayout";
+
+const UserProfilePage: NextPage = () => {
+  return (
+    <div>
+      <Twitterlayout>
+        <h1>proile page</h1>
+      </Twitterlayout>
+    </div>
+  );
+};
+
+export default UserProfilePage;
