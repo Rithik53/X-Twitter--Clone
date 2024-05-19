fb324d036bc28e015aa7d9a532a428d5b6c3c829
Rithik53 <rithikkumar53@gmail.com>
2024-05-20 02:32:14 +0530

    changed api urls

--- twitter-client/clients/api.ts
+++ twitter-client/clients/api.ts
@@ -1,10 +1,13 @@
-import {GraphQLClient} from 'graphql-request';
+import { GraphQLClient } from "graphql-request";
 
-const isClient = typeof window !=="undefined";
-export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql",{
-    headers:()=>({
-        Authorization: isClient
-         ?`Bearer ${window.localStorage.getItem('__twitter_token')}`
+const isClient = typeof window !== "undefined";
+export const graphqlClient = new GraphQLClient(
+  process.env.NEXT_PUBLIC_API_URL as string,
+  {
+    headers: () => ({
+      Authorization: isClient
+        ? `Bearer ${window.localStorage.getItem("__twitter_token")}`
         : "",
     }),
-});
+  }
+);
