4a542d0e9ff430127f4abfa76c37e511b93b248f
Rithik53 <rithikkumar53@gmail.com>
2024-05-20 02:58:44 +0530

    Update tailwind.config.ts

--- twitter-client/tailwind.config.ts
+++ twitter-client/tailwind.config.ts
@@ -1,34 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+type AddUtilitiesFunction = (utilities: Record<string, any>) => void;
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [
-    function({ addUtilities }){
-      const newUtilities ={
-        '.no-scrollbar::-webkit-scrollbar':{
+    function ({ addUtilities }: { addUtilities: AddUtilitiesFunction }) {
+      const newUtilities = {
+        ".no-scrollbar::-webkit-scrollbar": {
           display: "none",
         },
-        '.no-scrollbar':{
-          '-ms-overflow-style':"none",
-          'scrollbar-width': "none",
-        }
-
-      }
-      addUtilities(newUtilities)
-    }
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+      addUtilities(newUtilities);
+    },
   ],
-}
-export default config
+};
+
+export default config;
