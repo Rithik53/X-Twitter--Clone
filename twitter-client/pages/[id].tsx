1000b9d3582c8ad6753869f5554744c7ffa0de00
Rithik53 <rithikkumar53@gmail.com>
2024-05-19 22:02:19 +0530

    follow and user recommendation

--- twitter-client/pages/[id].tsx
+++ twitter-client/pages/[id].tsx
@@ -2,11 +2,18 @@ import { graphqlClient } from "@/clients/api";
 import FeedCard from "@/components/FeedCard";
 import Twitterlayout from "@/components/FeedCard/Layout/TwitterLayout";
 import { Tweet, User } from "@/gql/graphql";
+import {
+  followUserMutation,
+  unfollowUserMutation,
+} from "@/graphql/mutation/user";
 import { getUserByIdQuery } from "@/graphql/query/user";
 import { useCurrentUser } from "@/hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
 import { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+import toast from "react-hot-toast";
 import { BsArrowLeftShort } from "react-icons/bs";
 
 interface ServerProps {
@@ -14,6 +21,41 @@ interface ServerProps {
 }
 const UserProfilePage: NextPage<ServerProps> = (props) => {
   const router = useRouter();
+  const { user: currentUser } = useCurrentUser();
+  const queryClient = useQueryClient();
+
+  const amIfollowing = useMemo(() => {
+    if (!props.userInfo) return false;
+    return (
+      (currentUser?.following?.findIndex(
+        (el) => el?.id === props.userInfo?.id
+      ) ?? -1) >= 0
+    );
+  }, [currentUser?.following, props.userInfo]);
+
+  const handleFollowUser = useCallback(async () => {
+    if (!props.userInfo?.id) return;
+
+    try {
+      await graphqlClient.request(followUserMutation, {
+        to: props.userInfo.id,
+      });
+      await queryClient.invalidateQueries(["current-user"]);
+      toast.success("Followed user successfully", { id: "3" });
+    } catch (error) {
+      toast.error("Failed to follow user", { id: "3" });
+      console.error("Follow user error:", error);
+    }
+  }, [props.userInfo?.id, queryClient]);
+
+  const handleUnFollowUser = useCallback(async () => {
+    if (!props.userInfo?.id) return;
+
+    await graphqlClient.request(unfollowUserMutation, {
+      to: props.userInfo?.id,
+    });
+    await queryClient.invalidateQueries(["current-user"]);
+  }, [props.userInfo?.id, queryClient]);
   return (
     <div>
       <Twitterlayout>
@@ -21,7 +63,9 @@ const UserProfilePage: NextPage<ServerProps> = (props) => {
           <nav className="flex items-center gap-3 py-3 px-3 ">
             <BsArrowLeftShort className="text-4xl" />
             <div>
-              <h1 className="text-2xl font-bold">Rithik jha</h1>
+              <h1 className="text-2xl font-bold">
+                {props.userInfo?.firstName} {props.userInfo?.lastName}
+              </h1>
               <h1 className="text-md font-bold text-slate-500">
                 {props.userInfo?.tweets?.length} Tweets
               </h1>
@@ -37,7 +81,34 @@ const UserProfilePage: NextPage<ServerProps> = (props) => {
                 height={200}
               />
             )}
-            <h1 className="text-2xl font-bold mt-5">Rithik jha</h1>
+            <h1 className="text-2xl font-bold mt-5">
+              {props.userInfo?.firstName} {props.userInfo?.lastName}{" "}
+            </h1>
+            <div className="flex justify-between items-center">
+              <div className="flex gap-4 mt-2 text-sm text-gray-400">
+                <span> {props.userInfo?.followers?.length} Followers</span>
+                <span>{props.userInfo?.following?.length} Following</span>
+              </div>
+              {currentUser?.id !== props.userInfo?.id && (
+                <>
+                  {amIfollowing ? (
+                    <button
+                      onClick={handleUnFollowUser}
+                      className="bg-white text-black px-3 py-1 rounded-full text-sm"
+                    >
+                      UnFollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={handleFollowUser}
+                      className="bg-white text-black px-3 py-1 rounded-full text-sm"
+                    >
+                      Follow
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
           </div>
           <div>
             {props.userInfo?.tweets?.map((tweet) => (
--- twitter-server/build/app/user/resolvers.js
+++ twitter-server/build/app/user/resolvers.js
@@ -52,6 +52,30 @@ const extraResolvers = {
             });
             return result.map((el) => el.following);
         }),
+        recommendedUsers: (parent, _, ctx) => __awaiter(void 0, void 0, void 0, function* () {
+            if (!ctx.user)
+                return [];
+            const myFollowings = yield db_1.prismaclient.follows.findMany({
+                where: {
+                    follower: { id: ctx.user.id },
+                },
+                include: {
+                    following: {
+                        include: { followers: { include: { following: true } } },
+                    },
+                },
+            });
+            const users = [];
+            for (const followings of myFollowings) {
+                for (const followingOfFollowedUser of followings.following.followers) {
+                    if (followingOfFollowedUser.following.id !== ctx.user.id &&
+                        myFollowings.findIndex((e) => e.followingId === followingOfFollowedUser.following.id) < 0) {
+                        users.push(followingOfFollowedUser.following);
+                    }
+                }
+            }
+            return users;
+        }),
     },
 };
 const mutations = {
