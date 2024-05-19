ad137062318d36321265094f8674e24f3396d479
Rithik53 <rithikkumar53@gmail.com>
2024-05-20 02:41:25 +0530

    fixed bug

--- twitter-client/components/FeedCard/Layout/TwitterLayout.tsx
+++ twitter-client/components/FeedCard/Layout/TwitterLayout.tsx
@@ -91,7 +91,7 @@ const Twitterlayout: React.FC<TwitterlayoutProps> = (props) => {
       if (verifyGoogleToken)
         window.localStorage.setItem("__twitter_token", verifyGoogleToken);
 
-      await queryClient.invalidateQueries(["current-user"]);
+      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
     },
     [queryClient]
   );
